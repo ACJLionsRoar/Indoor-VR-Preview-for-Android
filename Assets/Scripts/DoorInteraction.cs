dad31d2430c01a93cb7367d4475f25fef43d88f3
Aswin CJ <aswincj4@gmail.com>
2017-03-19 20:34:13 +0530

    Gaze Input Added

---
+++ Assets/Scripts/DoorInteraction.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DoorInteraction : MonoBehaviour {
+
+	private float timer=0;
+	public float gazeTime = 2f;
+
+	private bool gazedAt;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (gazedAt == true) {
+			timer += Time.deltaTime;
+
+			if (timer >= gazeTime) {
+				ExecuteEvents.Execute (gameObject, new PointerEventData (EventSystem.current), ExecuteEvents.pointerDownHandler);
+				timer = 0f;
+
+//				GetComponent<Collider> ().enabled = false;
+
+			}
+		}
+		
+	}
+
+	public void PointerEnter(){
+		
+		gazedAt = true;
+	}
+
+	public void PointerExit(){
+		
+		gazedAt = false;
+	}
+
+//	public void PointerDown(){
+//		Debug.Log ("Pointer Down");
+//	}
+}

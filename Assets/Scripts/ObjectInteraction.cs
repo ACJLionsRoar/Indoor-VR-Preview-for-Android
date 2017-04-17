6d1f6e6986cdf55981db77a329886dcc52c6d2ca
Aswin CJ <aswincj4@gmail.com>
2017-04-17 16:14:32 +0530

    Pickup and Drop Objects Function Completed

---
+++ Assets/Scripts/ObjectInteraction.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ObjectInteraction : MonoBehaviour {
+
+	private float timer=0;
+	public float pickupTime = 3f;
+	public float dropTime = 6f;
+
+	private bool gazedAt;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (gazedAt == true) {
+			timer += Time.deltaTime;
+
+			if (timer >= pickupTime && timer <= dropTime) {
+				ExecuteEvents.Execute (gameObject, new PointerEventData (EventSystem.current), ExecuteEvents.pointerDownHandler);
+//				Debug.Log ("Picked Up Event");
+
+//				GetComponent<Collider> ().enabled = false;
+
+			} else if (timer > dropTime) {
+				ExecuteEvents.Execute (gameObject, new PointerEventData (EventSystem.current), ExecuteEvents.selectHandler);
+//				Debug.Log ("Dropped Event");
+				timer = 0f;
+			}
+		}
+		
+	}
+
+	public void PointerEnter(){
+		
+		gazedAt = true;
+	}
+
+	public void PointerExit(){
+		
+		gazedAt = false;
+	}
+
+//	public void PointerDown(){
+//		Debug.Log ("Pointer Down");
+//	}
+}
---
+++ Assets/Scripts/PickDrop.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickDrop : MonoBehaviour {
+
+	private bool pickedUp;
+	private GameObject Reticle;
+
+	private Color originalColor;
+
+	public int pickupDistance=4;
+
+//	private Material[] originalMaterial;
+//	private Material mat;
+
+	// Use this for initialization
+	void Start () {
+//		mat = new Material(Shader.Find("Specular"));
+//		mat.color = Color.blue;
+
+		pickedUp = false;
+		Reticle = GameObject.Find ("GvrReticle");
+		originalColor = GetComponent<Renderer> ().material.color;
+//		originalMaterial=GetComponent<Renderer>().materials;
+
+
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (pickedUp) {
+			Ray ray = new Ray (Reticle.transform.position, Reticle.transform.forward);
+			transform.position = ray.GetPoint (pickupDistance);
+		}
+		
+	}
+
+	public void PointerDown(){
+
+		pickedUp = true;
+	
+		GetComponent<Renderer> ().material.color = Color.blue;
+		
+//			GetComponent<Renderer>().materials[0]=mat;
+			
+	}
+
+	public void Select(){
+
+		pickedUp = false;
+		
+		GetComponent<Renderer> ().material.color = originalColor;
+	
+//			GetComponent<Renderer>().materials=originalMaterial;
+	}
+}
